8f9dcadf9e3f89293bb7819c7e3b90ff9e79a5f6
Christabelle143 <christabelleyouta143@gmail.com>
2022-05-10 18:22:51 +0100

    Modify backgroud color opacity and the blur effects

--- index.js
+++ index.js
@@ -1,26 +1,33 @@
-const logo = document.querySelector('.logo');
-const menu = document.querySelector('.menu3');
-const hamburger = document.querySelector('#openmenu');
-const closeHamburger = document.querySelector('#closemenu');
-const links = document.querySelector('.nav_items');
-const navLinks = document.querySelectorAll('.nav_items li');
-const lis = Array.from(navLinks);
+let logo = document.querySelector(".logo");
+let menu = document.querySelector(".menu3");
+let hamburger = document.querySelector('#openmenu');
+let closeHamburger = document.querySelector('#closemenu');
+let links= document.querySelector(".nav_items");
+let navLinks = document.querySelectorAll(".nav_items li");
+let about = document.querySelector(".infos");
+let lis = Array.from(navLinks);
 
-function toggler() {
-  logo.classList.toggle('hide');
-  hamburger.classList.toggle('remove');
-  document.body.classList.toggle('no-scroll');
-}
+function toggler(){
+    logo.classList.toggle("hide");
+    hamburger.classList.toggle('remove');
+    document.body.classList.toggle("no-scroll");
+    
+  }
 
-menu.addEventListener('click', () => {
-  toggler();
-  links.classList.toggle('hide');
-  closeHamburger.classList.toggle('remove');
-});
-for (let i = 0; i < lis.length; i += 1) {
-  lis[i].addEventListener('click', () => {
-    toggler();
-    links.classList.add('hide');
+menu.addEventListener("click", function(){
+    toggler();  
+    links.classList.toggle("hide");
+    closeHamburger.classList.toggle('remove')
+    about.classList.toggle("blur")
+  })
+
+  for (let i = 0; i<lis.length; i += 1){
+    lis[i].addEventListener("click", function(){
+        toggler();
+    links.classList.add("hide");  
     closeHamburger.classList.add('remove');
-  });
-}
+    about.classList.add("remove")
+   
+
+    })
+  }
